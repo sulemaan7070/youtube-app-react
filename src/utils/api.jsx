2154c9cb230c8dfc008fbad3e966bb29709f9423
sulemaan7070 <sulemaan7070@gmail.com>
2023-04-17 16:41:34 +0530

    almost completed

---
+++ src/utils/api.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+const arr = ["bollywood", "hollywood", "tollywood"];
+let val = arr[Math.floor(Math.random() * arr.length)];
+//randomly passing a paramter to the api to give the feeling of random home screen of youtube
+const options = {
+  params: { q: val, hl: "en", gl: "IN" },
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_SOME_KEY || "your api key",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
+
+console.log(import.meta.env.VITE_SOME_KEY);
